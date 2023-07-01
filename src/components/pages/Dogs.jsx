7c263ce1de1201a22378e03d829f453a0e500fd6
Iryna Kutsevol <117863253+irkutsio@users.noreply.github.com>
2023-07-01 13:50:08 +0300

    Update Dogs.jsx

--- src/components/pages/Dogs.jsx
+++ src/components/pages/Dogs.jsx
@@ -1,10 +1,32 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useSearchParams } from 'react-router-dom';
 
 const Dogs = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [dogs, setDogs] = useState(['🐕-1', '🐩-2', '🐕‍🦺-3', '🦮-4']);
+  const dogId = searchParams.get('dogId');
+
+const visibleDogs = dogs.filter(dog => dog.includes(dogId))
+
   // тут делать запрос на породы
   return (
     <div>
-      {['🐕', '🐩', '🐕‍🦺', '🦮'].map((item, idx) => {
+      <input
+        value={dogId ? dogId : ''}
+        type="text"
+        onChange={e => {
+          setSearchParams({ dogId: e.target.value });
+        }}
+      />
+      <button
+        type="button"
+        onClick={() => {
+          setSearchParams({ c: 'hello' });
+        }}
+      >
+        Change SP
+      </button>
+      {visibleDogs.map((item, idx) => {
         return (
           <Link key={idx} to={`${idx}`}>
             <h4>Dog breed:</h4>
